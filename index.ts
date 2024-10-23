29d34460b6b2e78f6d2af69c614f28640c35b749
Musa Mutetwi <musasoftlabx@gmail.com>
2024-10-23 10:38:51 +0300

    feat: list-to-provide-json-as-opposed-to-array

--- index.ts
+++ index.ts
@@ -175,7 +175,7 @@ const app = new Elysia()
   .get("/scan", () => scan())
   .get("/truncate", () => truncate())
   .get("/*", (params) => list(params))
-  .listen(3000);
+  .listen(6666);
 
 console.log(
   `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
