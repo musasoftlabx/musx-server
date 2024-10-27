ed93bac318049d7836a265cd4bbdf40524eb1e70
Musa Mutetwi <musasoftlabx@gmail.com>
2024-10-28 00:10:11 +0300

    feat: play-count

--- routes/plays.ts
+++ routes/plays.ts
@@ -1,6 +1,7 @@
 import { DB } from "..";
 
 export const plays = (params: { body: { id: number } }) => {
+  console.log("params:", params);
   const { id } = params.body;
   DB.query(`UPDATE directory SET plays = plays + 1 WHERE id = ${id}`).run();
   DB.query(`INSERT INTO plays VALUES (NULL, ${id}, DateTime('now'))`).run();
