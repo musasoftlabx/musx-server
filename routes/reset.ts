a29df134c1bd31fd84a286aaca7763c71f4923b2
Musa Mutetwi <musasoftlabx@gmail.com>
2024-10-29 06:36:25 +0300

    feat: splitting-routes+reset-library

--- routes/reset.ts
+++ routes/reset.ts
@@ -1,3 +1,4 @@
+import { exec } from "child_process";
 import { readdirSync } from "fs";
 import { emptydirSync, removeSync } from "fs-extra";
 
@@ -5,5 +6,6 @@ export default async function reset() {
   emptydirSync("./Artwork");
   emptydirSync("./Waveform");
   removeSync("./musx.db");
+  exec(`pm2 restart index`);
   return readdirSync("./").sort();
 }
