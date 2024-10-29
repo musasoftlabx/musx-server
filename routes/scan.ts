0aca3c8f831f528abb2030d2ef050fd8432dfef1
Musa Mutetwi <musasoftlabx@gmail.com>
2024-10-29 15:33:43 +0300

    feat: refining-scanner

--- routes/scan.ts
+++ routes/scan.ts
@@ -1,6 +1,6 @@
 import { DB } from "..";
 import { Glob } from "bun";
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import { existsSync } from "fs";
 import { Stream } from "@elysiajs/stream";
 import rgbHex from "rgb-hex";
@@ -23,75 +23,87 @@ export default async function scan() {
     for await (const entry of glob.scan(".")) {
       count++;
 
-      stream.send(`${count}. ${entry}`);
-
       const file = entry.replaceAll("$", "\\$").replaceAll("`", "\\`");
 
-      exec(
-        `ffprobe -show_entries 'stream:format' -output_format json "./${file}"`,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.error(`error: ${error.message}`);
-            return;
-          }
-
-          // ? If no errors,
-          const { streams, format } = JSON.parse(stdout);
-
-          // ? Remove root directory from entry
-          const path = file
-            .replace("Music/", "")
-            .replaceAll("\\$", "$")
-            .replaceAll("\\`", "`");
-
-          // ? Destructure
-          const {
-            tags,
-            bit_rate: bitrate,
-            size,
-            duration,
-            format_name,
-          } = format;
-
-          // ? Get the path and rename it to make artwork
-          const artwork = `${path
-            .replace(`.${format_name}`, "")
-            .replace(/[^a-zA-Z0-9]/g, "_")}.jpg`; //\W+
-
-          // ? Get the path and rename it to make waveform
-          const waveform = `${path
-            .replace(`.${format_name}`, "")
-            .replace(/[^a-zA-Z0-9]/g, "_")}.png`;
-
-          // ? Insert record to DB
-          try {
-            DB.query(
-              `INSERT INTO tracks VALUES (NULL,?,DateTime('now'),?,?,?,?,?,?,?,0,0,?,?,?,?,?,?,?,?,?,?,NULL,NULL)`
-            ).run([
-              path,
-              tags?.title,
-              tags?.album,
-              tags?.album_artist,
-              tags?.artist,
-              tags?.genre,
-              tags?.date,
-              tags?.track,
-              bitrate,
-              size,
-              duration,
-              format_name,
-              streams[0].channels,
-              streams[0].channel_layout,
-              streams[0].sample_rate,
-              streams[0]?.tags?.encoder,
-              artwork,
-              waveform,
-            ] as any);
-          } catch (err: any) {
-            console.log("DB: ", err.message);
-          }
-        }
+      const stdout: any = execSync(
+        `ffprobe -show_entries 'stream:format' -output_format json "./${file}"`
       );
+      const buf = Buffer.from(stdout);
+      const base64Data = buf.toString("base64");
+      const res = atob(base64Data);
+
+      // ? If no errors,
+      const { streams, format } = JSON.parse(res);
+
+      // ? Remove root directory from entry
+      const path = file
+        .replace("Music/", "")
+        .replaceAll("\\$", "$")
+        .replaceAll("\\`", "`");
+
+      // ? Destructure
+      const { tags, bit_rate: bitrate, size, duration, format_name } = format;
+
+      // ? Get the path and rename it to make artwork
+      const artwork = `${path
+        .replace(`.${format_name}`, "")
+        .replace(/[^a-zA-Z0-9]/g, "_")}.jpg`; //\W+
+
+      // ? Generate waveform
+      const waveform = `${path
+        .replace(`.${format_name}`, "")
+        .replace(/[^a-zA-Z0-9]/g, "_")}.png`;
+
+      const trackPath = `./Music/${path
+        .replaceAll("$", "\\$")
+        .replaceAll("`", "\\`")}`;
+      const artworkPath = `./Artwork/${artwork}`;
+      const waveformPath = `./Waveform/${waveform}`;
+
+      // ? Execute ffmpeg to construct waveform
+      if (!existsSync(waveformPath)) {
+        execSync(
+          `ffmpeg -y -i "${trackPath}" -filter_complex showwavespic -frames:v 1 "${waveformPath}"`
+        );
+      }
+
+      // ? Execute ffmpeg to extract artwork
+      if (!existsSync(artworkPath)) {
+        execSync(
+          `ffmpeg -y -i "${trackPath}" -an -vcodec copy "${artworkPath}"`
+        );
+      }
+
+      // ? Insert record to DB
+      try {
+        DB.query(
+          `INSERT INTO tracks VALUES (NULL,?,DateTime('now'),?,?,?,?,?,?,?,0,0,?,?,?,?,?,?,?,?,?,?,?,NULL)`
+        ).run([
+          path,
+          tags?.title,
+          tags?.album,
+          tags?.album_artist,
+          tags?.artist,
+          tags?.genre,
+          tags?.date,
+          tags?.track,
+          bitrate,
+          size,
+          duration,
+          format_name,
+          streams[0].channels,
+          streams[0].channel_layout,
+          streams[0].sample_rate,
+          streams[0]?.tags?.encoder,
+          artwork,
+          waveform,
+          await colorsFromImage(artworkPath),
+        ] as any);
+      } catch (err: any) {
+        console.log("DB:", err.message);
+      }
+
+      stream.send(`${count}. ${entry}`);
     }
 
     /* const paths: any = DB.query(
