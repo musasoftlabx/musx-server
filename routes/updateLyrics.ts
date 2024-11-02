54c90bb1c7c351ed6cdf4e31efdcc2d3ad9e9d1e
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-02 10:24:39 +0300

    feat: lyrics

--- routes/updateLyrics.ts
+++ routes/updateLyrics.ts
@@ -4,7 +4,7 @@ import { Lyrics } from "../types";
 export default function updateLyrics(params: Lyrics) {
   const { id, lyrics } = params.body;
   return DB.query(`UPDATE tracks SET lyrics = ? WHERE id = ?`).run([
-    lyrics,
+    atob(lyrics),
     id,
   ] as {});
 }
