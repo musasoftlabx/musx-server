9dad3f329648766705bcee029b9a367afc3600ee
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-14 05:06:46 +0300

    feat: create-playlist

--- routes/createPlaylist.ts
+++ routes/createPlaylist.ts
@@ -2,16 +2,41 @@ import { DB } from "..";
 
 type TParams = { body: any; set: any; error: any };
 
+type PlaylistProps = {
+  id: number;
+  name: string;
+  artwork: string;
+  createdOn: string;
+  modifiedOn: string;
+  tracks: {}[];
+};
+
+type PlaylistsProps = PlaylistProps[];
+
 export default async function createPlaylist(params: TParams) {
   const { body, set, error } = params;
-  const { name } = body;
+  const { name, trackId } = body;
 
   try {
-    DB.query(
+    // ? Create a playlist
+    const { lastInsertRowid } = DB.run(
       `INSERT INTO playlists VALUES (NULL, "${name}", NULL, DateTime('now'), DateTime('now'))`
-    ).run();
+    );
 
-    return DB.query(`SELECT * FROM playlists`).all();
+    // ? Add initial track to playlist
+    DB.run(
+      `INSERT INTO playlistsTracks VALUES (NULL, ${lastInsertRowid}, ${trackId}, NULL, NULL, DateTime('now'))`
+    );
+
+    let playlists: PlaylistsProps = <PlaylistsProps>(
+      DB.query(`SELECT * FROM playlists ORDER BY id DESC`).all()
+    );
+
+    playlists.forEach(({ id }, i) => {
+      playlists[i].tracks = DB.query(
+        `SELECT * FROM playlistsTracks WHERE playlistId = ${id}`
+      ).all();
+    });
   } catch (err: any) {
     return error(500, {
       subject: "Playlist Creation Error",
