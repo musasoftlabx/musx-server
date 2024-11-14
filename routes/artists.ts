ceb4b8a2279b597b9798ad80a68955cb49b67836
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-14 18:16:33 +0300

    feat: artists

--- routes/artists.ts
+++ routes/artists.ts
@@ -2,6 +2,6 @@ import { DB } from "..";
 
 export default function artists() {
   return DB.query(
-    `SELECT albumArtist, COUNT(albumArtist) AS tracks FROM tracks GROUP BY albumArtist ORDER BY tracks LIMIT 10`
+    `SELECT albumArtist, path, COUNT(albumArtist) AS tracks FROM tracks GROUP BY albumArtist ORDER BY tracks LIMIT 10`
   ).all();
 }
