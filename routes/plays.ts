cba6d8ffeb4326dd1763faa574e9624cbc12eac0
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-14 23:29:31 +0300

    feat: play-history

--- routes/plays.ts
+++ routes/plays.ts
@@ -10,7 +10,7 @@ export default function plays(params: TParams) {
 
   try {
     return DB.query(
-      `SELECT plays.id AS id, path, title, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder, artwork, waveform, palette, playedOn
+      `SELECT trackId AS id, path, title, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder, artwork, waveform, palette, playedOn
         FROM plays
         JOIN tracks
         ON trackId = tracks.id
