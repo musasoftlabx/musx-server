66ac41f9368a78f1ef2d2e56ba86bf13736f8b56
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-14 06:14:08 +0300

    feat: create-playlist

--- routes/playlists.ts
+++ routes/playlists.ts
@@ -2,9 +2,36 @@ import { DB } from "..";
 
 type TParams = { body: any; set: any; error: any };
 
-export default async function createPlaylist(params: TParams) {
+type PlaylistProps = {
+  id: number;
+  name: string;
+  artwork: string;
+  createdOn: string;
+  modifiedOn: string;
+  tracks: any;
+};
+
+type PlaylistsProps = PlaylistProps[];
+
+export default async function playlists(params: TParams) {
   const { body, set, error } = params;
   const { name } = body;
 
-  return DB.query(`SELECT * FROM playlists`).run();
+  // ? Get playlists
+  let playlists: PlaylistsProps = <PlaylistsProps>(
+    DB.query(`SELECT * FROM playlists ORDER BY id DESC`).all()
+  );
+
+  // ? Get playlist tracks for each playlist
+  playlists.forEach(({ id }, i) => {
+    playlists[i].tracks = DB.query(
+      `SELECT title, artwork 
+          FROM playlistTracks
+          JOIN tracks
+          ON playlistTracks.id = tracks.id
+          WHERE playlistId = ${id}`
+    ).all();
+  });
+
+  return playlists;
 }
