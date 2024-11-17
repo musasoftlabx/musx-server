ad9fa61e2378d1e7b0f437b48783fb35318a34b8
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-17 15:32:38 +0300

    feat: library-count

--- routes/libraryCount.ts
+++ routes/libraryCount.ts
@@ -2,9 +2,7 @@ import { DB } from "..";
 
 export default function libraryCount() {
   return {
-    artists: DB.query(
-      `SELECT COUNT(albumArtist) FROM tracks GROUP BY albumArtist`
-    ).values()[0][0],
+    artists: DB.query(`SELECT COUNT(albumArtist) FROM tracks`).values()[0][0],
     tracks: DB.query(`SELECT COUNT(id) FROM tracks`).values()[0][0],
     playlists: DB.query(`SELECT COUNT(id) FROM playlists`).values()[0][0],
   };
