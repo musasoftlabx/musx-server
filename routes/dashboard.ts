0efcf8c9e3e8e184b78686e548624af5c73a7c64
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-19 01:40:12 +0300

    feat: dashboard

--- routes/dashboard.ts
+++ routes/dashboard.ts
@@ -10,7 +10,7 @@ export const dashboard = () => {
   ).all();
 
   const recentlyPlayed = DB.query(
-    `SELECT DISTINCT path, title, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder, artwork, waveform, palette
+    `SELECT DISTINCT path, trackId, title, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder, artwork, waveform, palette
      FROM plays
      INNER JOIN tracks
      ON plays.trackId = tracks.id
