de80b7d7a56277735e6ed1ec3d2627e0eb17b2c9
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-19 22:49:31 +0300

    feat: return-plays-after-update

--- routes/updatePlayCount.ts
+++ routes/updatePlayCount.ts
@@ -11,7 +11,7 @@ export default function updatePlayCount(params: TParams) {
   try {
     DB.query(`UPDATE tracks SET plays = plays + 1 WHERE id = ${id}`).run();
     DB.query(`INSERT INTO plays VALUES (NULL, ${id}, DateTime('now'))`).run();
-    return true;
+    return DB.query(`SELECT rating FROM tracks WHERE id = ${id}`).get();
   } catch (err: any) {
     return error(500, {
       subject: "Update Error",
