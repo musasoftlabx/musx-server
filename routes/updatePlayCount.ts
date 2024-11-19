c25ffab1f65c4349ae2ce7395eba254e5f2e905a
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-19 05:03:30 +0300

    feat: dashboard

--- routes/updatePlayCount.ts
+++ routes/updatePlayCount.ts
@@ -1,9 +1,21 @@
 import { DB } from "..";
-import { PlayCount } from "../types";
 
-export default function updatePlayCount(params: PlayCount) {
-  const { id } = params.body;
-  DB.query(`UPDATE tracks SET plays = plays + 1 WHERE id = ${id}`).run();
-  DB.query(`INSERT INTO plays VALUES (NULL, ${id}, DateTime('now'))`).run();
-  return true;
+type TParams = { error: any; params: { id: string } };
+
+export default function updatePlayCount(params: TParams) {
+  const {
+    error,
+    params: { id },
+  } = params;
+
+  try {
+    DB.query(`UPDATE tracks SET plays = plays + 1 WHERE id = ${id}`).run();
+    DB.query(`INSERT INTO plays VALUES (NULL, ${id}, DateTime('now'))`).run();
+    return true;
+  } catch (err: any) {
+    return error(500, {
+      subject: "Update Error",
+      body: err.message,
+    });
+  }
 }
