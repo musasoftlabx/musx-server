0d130b5701d52e483ece116813886e26ce1ae436
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-22 04:32:12 +0300

    feat: artist-details

--- routes/artist.ts
+++ routes/artist.ts
@@ -1,4 +1,5 @@
 import { DB } from "..";
+import byteSize from "byte-size";
 
 type TParams = { error: any; params: { artist: string } };
 
@@ -17,7 +18,13 @@ export default async function artist(params: TParams) {
       `SELECT * FROM tracks WHERE albumArtist = ? GROUP BY album HAVING COUNT(album) = 1`
     ).all([decodeURIComponent(artist)] as any);
 
-    return { albums, singles };
+    return {
+      albums: albums.map((album: any) => ({
+        ...album,
+        size: `${byteSize(album.size)}`,
+      })),
+      singles,
+    };
   } catch (err: any) {
     return error(500, {
       subject: "Artist Album Retrieval Error",
