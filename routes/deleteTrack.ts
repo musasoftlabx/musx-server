df9fc294bcdcb61f94a9d70bac39eeab28562d17
Musa Mutetwi <musasoftlabx@gmail.com>
2024-11-25 02:37:57 +0300

    feat: delete-track

--- routes/deleteTrack.ts
+++ routes/deleteTrack.ts
@@ -2,22 +2,27 @@ import { DB } from "..";
 
 import { unlink } from "node:fs/promises";
 
-type TParams = { body: any; set: any; error: any };
+type TParams = { params: { id: string }; error: any };
 
 export default async function deleteTrack(params: TParams) {
-  const { body, set, error } = params;
-  const { id } = body;
+  const {
+    params: { id },
+    error,
+  } = params;
 
   const { path, artwork, waveform }: any = DB.query(
     `SELECT path, artwork, waveform FROM tracks WHERE id = ${id}`
   ).get();
 
-  try {
-    await unlink(`./Music/${path}`);
-    await unlink(`./Artwork/${artwork}`);
-    await unlink(`./Waveform/${waveform}`);
-    DB.query(`DELETE FROM tracks WHERE id = ${id}`).run();
-  } catch (err: any) {
-    return error(500, `${err.message}: ${path}`);
-  }
+  console.log({ path, artwork, waveform });
+  return { path, artwork, waveform };
+
+  // try {
+  //   await unlink(`./Music/${path}`);
+  //   await unlink(`./Artwork/${artwork}`);
+  //   await unlink(`./Waveform/${waveform}`);
+  //   return DB.query(`DELETE FROM tracks WHERE id = ${id}`).run();
+  // } catch (err: any) {
+  //   return error(500, `${err.message}: ${path}`);
+  // }
 }
