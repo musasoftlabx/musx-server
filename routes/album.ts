373c7c31283aabb8760f5af7fd0ffbd383bf7773
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-01 15:15:48 +0300

    fix: album-define-columns

--- routes/album.ts
+++ routes/album.ts
@@ -1,5 +1,7 @@
 import { DB } from "..";
 
+import { AUDIO_URL, ARTWORK_URL, WAVEFORM_URL } from "..";
+
 type TParams = { error: any; params: { artist: string; album: string } };
 
 export default async function artist(params: TParams) {
@@ -10,7 +12,15 @@ export default async function artist(params: TParams) {
 
   try {
     return DB.query(
-      `SELECT * FROM tracks WHERE albumArtist = ? AND album = ?`
+      `SELECT
+        id,
+        ('${AUDIO_URL}' || path) AS path,
+        syncDate, title, album, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder,
+        ('${ARTWORK_URL}' || artwork) AS artwork,
+        ('${WAVEFORM_URL}' || waveform) AS waveform,
+        palette
+      FROM tracks
+      WHERE albumArtist = ? AND album = ?`
     ).all([decodeURIComponent(artist), decodeURIComponent(album)] as any);
   } catch (err: any) {
     return error(500, {
