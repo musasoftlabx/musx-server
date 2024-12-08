21fe84ea4f4e10777d8646ece526b6e6441055e8
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-08 15:45:32 +0300

    dev: playlists

--- routes/playlist.ts
+++ routes/playlist.ts
@@ -1,4 +1,4 @@
-import { DB, tracksTableColumns } from "..";
+import { ARTWORK_URL, AUDIO_URL, DB, WAVEFORM_URL } from "..";
 
 type TParams = { error: any; params: { id: string } };
 
@@ -10,11 +10,17 @@ export default function playlist(params: TParams) {
 
   try {
     return DB.query(
-      `SELECT tracks.id AS id, ${tracksTableColumns}
-        FROM playlistTracks
-        JOIN tracks
-        ON trackId = tracks.id
-        WHERE playlistId = ${Number(id)}`
+      `SELECT
+        tracks.id id,
+        ('${AUDIO_URL}' || path) AS url,
+        path, syncDate, title, album, albumArtist, artists, genre, year, track, rating, plays, bitrate, size, duration, format, channels, channelLayout, sampleRate, encoder,
+        ('${ARTWORK_URL}' || artwork) AS artwork,
+        ('${WAVEFORM_URL}' || waveform) AS waveform,
+        palette
+      FROM playlistTracks
+      JOIN tracks
+      ON trackId = tracks.id
+      WHERE playlistId = ${Number(id)}`
     ).all();
   } catch (err: any) {
     return error(500, {
