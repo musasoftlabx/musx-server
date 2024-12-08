d6d30ec6a4ee19525d51c992a3fbb176e2540715
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-08 14:17:41 +0300

    dev: playlists

--- routes/playlists.ts
+++ routes/playlists.ts
@@ -1,5 +1,7 @@
 import { ARTWORK_URL, DB } from "..";
 
+import byteSize from "byte-size";
+
 type PlaylistProps = {
   id: number;
   name: string;
@@ -7,6 +9,8 @@ type PlaylistProps = {
   createdOn: string;
   modifiedOn: string;
   tracks: any;
+  duration: number;
+  size: string;
 };
 
 type PlaylistsProps = PlaylistProps[];
@@ -19,6 +23,24 @@ export default function playlists() {
 
   // ? Get playlist tracks for each playlist
   playlists.forEach(({ id }, i) => {
+    playlists[i].size = `${byteSize(
+      DB.query(
+        `SELECT SUM(size) size
+      FROM playlistTracks
+      JOIN tracks
+      ON trackId = tracks.id
+      WHERE playlistId = ${id}`
+      ).values()[0][0] as number
+    )}`;
+
+    playlists[i].duration = DB.query(
+      `SELECT SUM(duration) duration
+      FROM playlistTracks
+      JOIN tracks
+      ON trackId = tracks.id
+      WHERE playlistId = ${id}`
+    ).values()[0][0] as number;
+
     playlists[i].tracks = DB.query(
       `SELECT 
         title,
