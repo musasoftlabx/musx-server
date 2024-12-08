cd0f88c524b72b9f1b323325712a96407f17126f
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-08 03:58:44 +0300

    dev: search-history

--- routes/searchHistory.ts
+++ routes/searchHistory.ts
@@ -1,7 +1,7 @@
 import { DB } from "..";
 
 export default function searchHistory() {
-  return DB.query(`SELECT query FROM searchHistory LIMIT 20`)
+  return DB.query(`SELECT query FROM searchHistory ORDER BY id DESC LIMIT 20`)
     .all()
     .map(({ query }: any) => query);
 }
