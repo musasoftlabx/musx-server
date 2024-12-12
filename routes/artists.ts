ce23df406ebafe9a976002f95f7b80e531bc1070
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-12 11:40:41 +0300

    dev: artists

--- routes/artists.ts
+++ routes/artists.ts
@@ -3,7 +3,7 @@ import { ARTWORK_URL, AUDIO_URL, DB } from "..";
 export default function artists() {
   return DB.query(
     //`SELECT albumArtist, path, COUNT(albumArtist) AS tracks, SUM(plays) AS plays FROM tracks GROUP BY albumArtist, plays ORDER BY tracks`
-    `SELECT ('${AUDIO_URL}' || path) url, albumArtist, path, COUNT(albumArtist) AS tracks FROM tracks GROUP BY albumArtist ORDER BY albumArtist`
+    `SELECT ('${AUDIO_URL}' || path) url, albumArtist, path, genre, COUNT(albumArtist) AS tracks FROM tracks GROUP BY albumArtist ORDER BY albumArtist`
   )
     .all()
     .map((artist: any) => {
