0809a3e7d369471a70028702a555d843a4f7f352
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-18 03:39:57 +0300

    dev: playlist

--- routes/rearrangePlaylist.ts
+++ routes/rearrangePlaylist.ts
@@ -1,59 +1,23 @@
-import { ARTWORK_URL, DB } from "..";
-
-import byteSize from "byte-size";
-
-type PlaylistProps = {
-  id: number;
-  name: string;
-  createdOn: string;
-  modifiedOn: string;
-  tracks: number;
-  artworks: string[];
-  duration: string;
-  size: string;
-};
-
-const formatTime2 = (seconds: number) =>
-  new Date(seconds * 1000).toLocaleTimeString("en-GB", {
-    timeZone: "Etc/UTC",
-    hour12: false,
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-  });
-
-function formatTime(seconds: number) {
-  return [
-    Math.floor(seconds / 60 / 60),
-    Math.floor((seconds / 60) % 60),
-    Math.floor(seconds % 60),
-  ]
-    .join(":")
-    .replace(/\b(\d)\b/g, "0$1");
-}
+import { DB } from "..";
 
 export type RearrangePlaylist = {
   error: any;
-  body: {
-    playlistId: number;
-    from: { id: number; position: number };
-    to: { id: number; position: number };
-  };
+  body: { playlistId: number; trackIds: number[] };
 };
 
 export default function rearrangePlaylist(params: RearrangePlaylist) {
   const {
-    error,
-    body: { playlistId, from, to },
+    body: { playlistId, trackIds },
   } = params;
 
-  DB.exec(
-    `UPDATE playlistTracks SET position = ${from.pos} WHERE trackId = ${from.id} AND playlistId = ${playlistId}`
-  );
+  let position = 1;
 
-  DB.exec(
-    `UPDATE playlistTracks SET position = ${to.pos} WHERE trackId = ${to.id} AND playlistId = ${playlistId}`
-  );
+  for (const trackId of trackIds) {
+    DB.exec(
+      `UPDATE playlistTracks SET position = ${position} WHERE trackId = ${trackId} AND playlistId = ${playlistId}`
+    );
+    position++;
+  }
 
-  return { playlistId, from, to };
+  return trackIds;
 }
