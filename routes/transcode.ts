65a9d4194e3459913411a835c5cf79dc066f1759
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-20 01:45:28 +0300

    dev: transcoding

--- routes/transcode.ts
+++ routes/transcode.ts
@@ -23,7 +23,7 @@ export default async function transcode(params: Transcode) {
 
   try {
     // ? Convert the track and store it in the directory
-    execSync(`ffmpeg -i "${mp3Path}" -c:a libopus -b:a 64k "${oggPath}"`);
+    execSync(`ffmpeg -i "${mp3Path}" -c:a "${oggPath}"`);
     // ? Send file to client
     return Bun.file(oggPath);
   } catch (err: any) {
