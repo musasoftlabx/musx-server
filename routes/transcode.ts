d68458d9744daa4e2877996a61b0e27da00c5877
Musa Mutetwi <musasoftlabx@gmail.com>
2024-12-19 15:18:48 +0300

    dev: transcoding

--- routes/transcode.ts
+++ routes/transcode.ts
@@ -13,7 +13,7 @@ export default async function transcode(params: Transcode) {
   const mp3Path = `Music/${path}`;
   const oggPath = `${transcodeDir}/${path.split("/").slice(-1)}`.replace(
     ".mp3",
-    ".ogg"
+    ".opus"
   );
 
   // ? Check if Transcodes directory exists. If not create it
@@ -21,7 +21,7 @@ export default async function transcode(params: Transcode) {
   // ? Empty the directory
   emptyDirSync(`${transcodeDir}`);
   // ? Convert the track and store it in the directory
-  execSync(`ffmpeg -i "${mp3Path}" -c:a libvorbis -b:a 64k "${oggPath}"`);
+  execSync(`ffmpeg -i "${mp3Path}" -c:a libopus -b:a 64k "${oggPath}"`);
   // ? Send file to client
   return Bun.file(oggPath);
 }
