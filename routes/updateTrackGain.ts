ac30917791d00c4bf7769ee62be2bd166b27c033
Musa Mutetwi <musasoftlabx@gmail.com>
2025-01-03 14:48:50 +0300

    Dev: Update track gain

--- routes/updateTrackGain.ts
+++ routes/updateTrackGain.ts
@@ -4,15 +4,13 @@ import dayjs from "dayjs";
 
 export type TrackGain = {
   error: any;
-  params: { body: { trackId: number; path: string; decibels: number } };
+  body: { trackId: number; path: string; decibels: number };
 };
 
 export default function updateTrackGain(params: TrackGain) {
   const {
     error,
-    params: {
-      body: { trackId, path, decibels },
-    },
+    body: { trackId, path, decibels },
   } = params;
 
   console.log(params);
@@ -21,12 +19,11 @@ export default function updateTrackGain(params: TrackGain) {
 
   try {
     execSync(`ffmpeg -i ${audioPath} -af "volume=${decibels}dB" ${audioPath}`);
-    return params;
-    // return DB.exec(`INSERT INTO trackGains VALUES (NULL, ?, ?, NULL, ?)`, [
-    //   trackId,
-    //   decibels,
-    //   dayjs().format("YYYY-MM-DD HH:mm:ss"),
-    // ]);
+    return DB.exec(`INSERT INTO trackGains VALUES (NULL, ?, ?, NULL, ?)`, [
+      trackId,
+      decibels,
+      dayjs().format("YYYY-MM-DD HH:mm:ss"),
+    ]);
   } catch (err: any) {
     return error(404, err.message);
   }
