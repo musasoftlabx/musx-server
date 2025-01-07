bf05592536f2d1b8ab4ff516221d59bf183381af
Musa Mutetwi <musasoftlabx@gmail.com>
2025-01-08 01:59:11 +0300

    Fix: Transcode missing chunks

--- routes/transcode.ts
+++ routes/transcode.ts
@@ -38,7 +38,7 @@ export default async function transcode(params: Transcode) {
               ${conversion} \
               -hls_time 1 \
               -hls_flags independent_segments \
-              -hls_segment_filename ${transcodeHeader}%03d.ts \
+              -hls_segment_filename "${transcodeHeader}%03d.ts" \
               -hls_list_size ${duration} \
               -f hls \
               "${transcodeHeaderFile}"
