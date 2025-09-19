334e543aa043d7920745c8ff9215303a01b16219
Musa Mutetwi <musasoftlabx@gmail.com>
2025-09-19 08:18:41 +0300

    Feat: Edit Playlist

--- routes/editPlaylist.ts
+++ routes/editPlaylist.ts
@@ -4,12 +4,12 @@ export type PlaylistProps = { body: any; set: any; error: any };
 
 export default async function editPlaylist(params: PlaylistProps) {
   const { body, set, error } = params;
-  const { name, artwork, trackId } = body;
+  const { name, artwork, id } = body;
 
   try {
     DB.run(
       `UPDATE playlists SET name = ?, artwork = ?, modifiedOn = DateTime('now') WHERE id = ?`,
-      [name, artwork, trackId]
+      [name, artwork, id]
     );
   } catch (err: any) {
     return error(500, {
