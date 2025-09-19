6f3703d3837070563e2e339903508a0253b2358c
Musa Mutetwi <musasoftlabx@gmail.com>
2025-09-19 04:54:10 +0300

    AI test 3

--- routes/generateImageAI.ts
+++ routes/generateImageAI.ts
@@ -14,16 +14,13 @@ export default async function generateImageAI(params: Prompt) {
 
   try {
     const client = new OpenAI({
-      apiKey:
-        "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",
+      apiKey: "",
     });
 
-    const response = await client.responses.create({
+    return await client.responses.create({
       model: "gpt-4.1",
       input: "Tell me a three sentence bedtime story about a unicorn.",
     });
-
-    return response;
 
     // const img = await client.images.generate({
     //   model: "gpt-image-1",
