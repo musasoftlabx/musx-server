73c0797b08ee3087088800764915fe7d168b89f4
Musa Mutetwi <musasoftlabx@gmail.com>
2025-10-11 11:41:08 +0300

    Feat: Create New Playlist

--- routes/createPlaylist.ts
+++ routes/createPlaylist.ts
@@ -3,6 +3,7 @@ import { DB } from "..";
 type TParams = { body: any; set: any; error: any };
 
 export default async function createPlaylist(params: TParams) {
+  console.log(params);
   const {
     body: { name, description },
     set,
@@ -14,19 +15,17 @@ export default async function createPlaylist(params: TParams) {
       `SELECT COUNT(id) FROM playlists WHERE name = "${name}"`
     ).values()[0][0];
 
-    console.log("exists", exists);
-
-    if (exists)
-      return error(403, {
-        subject: "Playlist Creation Error",
-        body: "A playlist with a similar name already exists",
-      });
-    else {
+    if (exists === 0) {
       const { lastInsertRowid } = DB.run(
         `INSERT INTO playlists VALUES (NULL, "${name}", "${description}", NULL, DateTime('now'), DateTime('now'))`
       );
 
       return lastInsertRowid;
+    } else {
+      return error(403, {
+        subject: "Playlist Creation Error",
+        body: "A playlist with a similar name already exists",
+      });
     }
   } catch (err: any) {
     return error(500, {
